0007d0b80423c2d899f916e7c8535bf5df7febe7
fzb <2469081876@qq.com>
2019-11-18 12:03:30 +0800

    add index

--- src/test/java/fzb/community/community/CommunityApplicationTests.java
+++ src/test/java/fzb/community/community/CommunityApplicationTests.java
@@ -3,11 +3,14 @@ package fzb.community.community;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
 @SpringBootTest
 class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
